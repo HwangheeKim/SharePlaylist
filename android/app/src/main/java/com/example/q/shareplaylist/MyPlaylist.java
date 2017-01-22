50c92daf378e2c3a480ace1abe793798676c4eaa
Yunyol-Shin <yunyol.shin@gmail.com>
2017-01-22 23:48:27 +0900

    playlist fix

--- android/app/src/main/java/com/example/q/shareplaylist/MyPlaylist.java
+++ android/app/src/main/java/com/example/q/shareplaylist/MyPlaylist.java
@@ -7,23 +7,33 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MyPlaylist extends Fragment {
+    static final int RESULT_OK = 1;
     View rootView;
     FloatingActionButton fab;
 
-
-
+    ListView listView;
+    VideoAdapter adapter;
+    String youtubeKey = "AIzaSyBOmiAJ9FD_IWza61CHPJCzZb8lj3gggrA";
 
 
     @Override
@@ -31,10 +41,27 @@ public class MyPlaylist extends Fragment {
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_my_playlist, container, false);
 
-        ListView listview = (ListView) rootView.findViewById(R.id.myPlayListView);
+        listView = (ListView) rootView.findViewById(R.id.myPlayListView);
+        adapter = new VideoAdapter();
+        listView.setAdapter(adapter);
 
-
-
+        // Load Playlist From Server
+        Ion.with(this).load(MainActivity.serverURL+"/user/" + MainActivity.userID)
+                .asJsonObject().setCallback(new FutureCallback<JsonObject>() {
+            @Override
+            public void onCompleted(Exception e, JsonObject result) {
+                adapter.clear();
+                JsonArray items = result.get("playlist").getAsJsonArray();
+                for(int i=0 ; i<items.size() ; i++) {
+                    JsonObject item = items.get(i).getAsJsonObject();
+                    try{adapter.add(item.get("url").getAsString(),
+                            URLDecoder.decode(item.get("title").getAsString(), "utf-8"),
+                            URLDecoder.decode(item.get("uploader").getAsString(), "utf-8"),
+                            item.get("thumbnail").getAsString());}catch(Exception d){d.printStackTrace();}
+                }
+                Log.d("Load user's playlist", result.toString());
+            }
+        });
 
 
 
@@ -46,15 +73,16 @@ public class MyPlaylist extends Fragment {
             @Override
             public void onClick(View v) {
                 Intent searchIntent = new Intent(getActivity(), loaderActivity.class);
-                startActivity(searchIntent);
+                getActivity().startActivityForResult(searchIntent, MainActivity.SELECT_UPLOAD);
             }
         });
 
-
-
-
-
-
         return rootView;
     }
+
+
+
+
+
+
 }
