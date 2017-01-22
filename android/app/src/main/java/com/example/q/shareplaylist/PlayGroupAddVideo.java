2bc38a337f44321f1bba0d576c678b7b50c993dc
HwangheeKim <hwangheek@gmail.com>
2017-01-22 18:31:42 +0900

    AddVideo Youtube Search Implemented

--- android/app/src/main/java/com/example/q/shareplaylist/PlayGroupAddVideo.java
+++ android/app/src/main/java/com/example/q/shareplaylist/PlayGroupAddVideo.java
@@ -1,25 +1,37 @@
 package com.example.q.shareplaylist;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
+import java.net.URLEncoder;
+
 public class PlayGroupAddVideo extends Fragment {
     View rootView;
+    EditText editText;
+    ListView listView;
+    VideoAdapter adapter;
+    String youtubeKey = "AIzaSyBOmiAJ9FD_IWza61CHPJCzZb8lj3gggrA";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_play_group_add_video, container, false);
+        editText = (EditText)rootView.findViewById(R.id.playgroup_add_search);
+
+        listView = (ListView)rootView.findViewById(R.id.playgroup_add_list);
+        adapter = new VideoAdapter();
+        listView.setAdapter(adapter);
 
         // Load Playlist From Server
         Ion.with(getContext()).load(MainActivity.serverURL+"/playlist/" + MainActivity.userID)
@@ -32,10 +44,44 @@ public class PlayGroupAddVideo extends Fragment {
             }
         });
 
-        // TODO : URGENT, From here!
         // Request Search query
-        // (in callback method) Load search result to the list
-
+        rootView.findViewById(R.id.playgroup_add_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Load search result to the list
+                youtubeSearch();
+            }
+        });
         return rootView;
+    }
+
+    void youtubeSearch() {
+        String keyword="";
+        try {
+            keyword = URLEncoder.encode(editText.getText().toString(), "utf-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String url = "https://www.googleapis.com/youtube/v3/search?part=snippet&order=viewCount&q="+keyword+"&type=video&videoDefinition=any&maxResults=20&key=" + youtubeKey;
+
+        Ion.with(getContext()).load(url).asJsonObject()
+                .setCallback(new FutureCallback<JsonObject>() {
+                    @Override
+                    public void onCompleted(Exception e, JsonObject result) {
+                        adapter.clear();
+                        Log.d("Youtube Search Result", result.toString());
+                        JsonArray items = result.get("items").getAsJsonArray();
+                        for(int i=0 ; i<items.size() ; i++) {
+                            JsonObject item = items.get(i).getAsJsonObject();
+                            JsonObject snippet = item.get("snippet").getAsJsonObject();
+                            adapter.add(item.get("id").getAsJsonObject().get("videoId").getAsString(),
+                                    snippet.get("title").getAsString(),
+                                    snippet.get("channelTitle").getAsString(),
+                                    snippet.get("thumbnails").getAsJsonObject()
+                                        .get("high").getAsJsonObject()
+                                        .get("url").getAsString());
+                        }
+                    }
+                });
     }
 }
