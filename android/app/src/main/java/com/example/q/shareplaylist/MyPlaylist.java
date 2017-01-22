da1911bd8fcc2416eabd5ef488b2d86be71fa0f4
Yunyol-Shin <yunyol.shin@gmail.com>
2017-01-22 18:42:29 +0900

    Fragment show

--- android/app/src/main/java/com/example/q/shareplaylist/MyPlaylist.java
+++ android/app/src/main/java/com/example/q/shareplaylist/MyPlaylist.java
@@ -1,20 +1,71 @@
 package com.example.q.shareplaylist;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyPlaylist extends Fragment {
     View rootView;
+    FloatingActionButton fab;
+
+
+
+    public class loaderActivity extends Activity {
+
+        @Override
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.fragment_loader);
+
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.fragment_loader, new PlayGroupAddVideo(), "tag1");
+            fragmentTransaction.commit();
+
+        }
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_my_playlist, container, false);
+
+        ListView listview = (ListView) rootView.findViewById(R.id.myPlayListView);
+
+
+
+
+
+
+        fab = (FloatingActionButton) rootView.findViewById(R.id.floatingActionButton);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent searchIntent = new Intent(getActivity(), loaderActivity.class);
+                startActivity(searchIntent);
+            }
+        });
+
+
+
+
+
 
         return rootView;
     }
